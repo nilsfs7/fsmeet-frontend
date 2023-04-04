33ec718b31aaa1c97704d792dff64c55c9b056b9
Nils Effinghausen <nils.effinghausen@telekom.de>
2023-04-04 20:53:59 +0200

    add fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Poly } from 'next/font/google';
+
+const poly = Poly({ subsets: ['latin'], weight: ['400'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={poly.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
