a05982373196d520257ac0746ea09b6802dc167d
Nils Effinghausen <nils.effinghausen@telekom.de>
2023-04-07 22:50:08 +0200

    receive result data from backend

--- pages/results.tsx
+++ pages/results.tsx
@@ -1,27 +1,19 @@
 import PlayerResult from '@/components/PlayerResult';
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
 
-const Results: NextPage = () => {
+const Results: NextPage = (props: any) => {
+  const results = props.data;
+
   return (
     <div>
       <h1 className="flex justify-center pb-24 pt-32 text-7xl">Battle Result</h1>
       <div className="grid grid-cols-2">
         <div className="flex justify-center">
-          <PlayerResult
-            name="Nils"
-            score={[13, 5, 4, 4]}
-            image="https://scontent-muc2-1.xx.fbcdn.net/v/t31.18172-8/17492379_1395721013824677_2431623315541165382_o.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_ohc=HxVgLT_npx4AX-WRSrd&_nc_ht=scontent-muc2-1.xx&oh=00_AfDNqfcc7VuvR0-bjrGcEHQA4Om_dOKr7xiHiS2Hu6-7Fg&oe=645399B7"
-            imageLeft={true}
-          />
+          <PlayerResult name={results[0].name} score={results[0].score} image={results[0].image} imageLeft={true} />
         </div>
 
         <div className="flex justify-center">
-          <PlayerResult
-            name="Samu"
-            score={[9, 4, 3, 2]}
-            image="https://static.wixstatic.com/media/beedc1_dea19613168348f0b71c124cf29309f2~mv2.jpg/v1/fill/w_1322,h_1480,al_b,q_85,usm_0.66_1.00_0.01,enc_auto/beedc1_dea19613168348f0b71c124cf29309f2~mv2.jpg"
-            imageLeft={false}
-          />
+          <PlayerResult name={results[1].name} score={results[1].score} image={results[1].image} imageLeft={false} />
         </div>
       </div>
       <h1 className="flex justify-center pt-24 text-7xl">Nils wins!</h1>
@@ -30,3 +22,14 @@ const Results: NextPage = () => {
 };
 
 export default Results;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await fetch('http://localhost:3000/v1/results/current');
+  const data = await response.json();
+
+  return {
+    props: {
+      data: data,
+    },
+  };
+};
