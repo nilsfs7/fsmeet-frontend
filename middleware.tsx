6ed63ff04236de96e3c31880335dad01d087c11a
Nils Effinghausen <nils.effinghausen@telekom.de>
2023-04-21 08:38:44 +0200

    fix redirects without jwt

--- middleware.tsx
+++ middleware.tsx
@@ -14,5 +14,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/jury/:path*',
+  matcher: ['/jury/:path*', '/account/:path*'],
 };
