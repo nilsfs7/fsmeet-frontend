bc9ce97e909ec2ffb21d4e39dd295b4285b4a5d7
Nils Effinghausen <nils.effinghausen@telekom.de>
2023-04-21 09:31:43 +0200

    fox open account page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import Button from '@/components/common/Button';
 import { NextPage } from 'next';
 import Link from 'next/link';
+import router from 'next/router';
 
 const CreateJury: NextPage = () => {
   const imgUrl =
     'https://scontent-muc2-1.xx.fbcdn.net/v/t31.18172-8/17492379_1395721013824677_2431623315541165382_o.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_ohc=HxVgLT_npx4AX-WRSrd&_nc_ht=scontent-muc2-1.xx&oh=00_AfDNqfcc7VuvR0-bjrGcEHQA4Om_dOKr7xiHiS2Hu6-7Fg&oe=645399B7';
+
+  const onClickProfile = (e: any) => {
+    router.push('/account');
+  };
 
   return (
     <>
@@ -26,12 +31,12 @@ const CreateJury: NextPage = () => {
         </div>
       </div>
 
-      {/* avatar */}
-      <Link href={'/account'}>
-        <div className="border-2border-[2px] absolute right-6 top-6 h-11 w-11 cursor-pointer rounded-full border-black bg-zinc-300  hover:bg-zinc-400">
-          <img alt={'nftImage'} className="h-full w-full rounded-full object-cover" src={imgUrl} />
-        </div>
-      </Link>
+      {/* profile */}
+      <div className="border-2border-[2px] absolute right-6 top-6 h-11 w-11 cursor-pointer rounded-full border-black bg-zinc-300  hover:bg-zinc-400">
+        <button className="h-full w-full rounded-full" onClick={onClickProfile}>
+          <img alt={'nftImage'} src={imgUrl} className="h-full w-full rounded-full object-cover" />
+        </button>
+      </div>
     </>
   );
 };
