f06983c21594b54a244e8e16083af16044efe7a7
Nils Effinghausen <nils.effinghausen@telekom.de>
2023-04-26 17:03:41 +0200

    avaoid absolute position for avatar

--- components/user/profile.tsx
+++ components/user/profile.tsx
@@ -1,18 +1,16 @@
 import { useSession } from 'next-auth/react';
 import router from 'next/router';
 
-interface IProfile {
-  imgUrl?: string;
-  username?: string;
-  onClick?: () => void;
-}
-
 const defaultImg = '/profile/default-pfp.png';
 const routeLogin = '/login';
 const routeAccount = '/account';
 
-const Profile = ({ imgUrl, onClick }: IProfile) => {
+const Profile = () => {
   const { data: session, status } = useSession();
+
+  // get from session
+  const imgUrl =
+    'https://scontent-muc2-1.xx.fbcdn.net/v/t31.18172-8/17492379_1395721013824677_2431623315541165382_o.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_ohc=HxVgLT_npx4AX-WRSrd&_nc_ht=scontent-muc2-1.xx&oh=00_AfDNqfcc7VuvR0-bjrGcEHQA4Om_dOKr7xiHiS2Hu6-7Fg&oe=645399B7';
 
   const onClickProfile = (e: any) => {
     isAuthenticated() ? router.push(routeAccount) : router.push(routeLogin);
@@ -26,8 +24,8 @@ const Profile = ({ imgUrl, onClick }: IProfile) => {
     <div className="grid min-w-[100px] cursor-pointer rounded-lg border-2 border-black bg-zinc-300 p-1 hover:bg-zinc-400">
       <button className="h-full w-full" onClick={onClickProfile}>
         <div className="grid grid-flow-col items-center">
-          <img alt={'user'} src={isAuthenticated() ? imgUrl : defaultImg} className="mx-2 h-11 w-11 rounded-full object-cover" />
-          <div className="mx-2 text-xl">{isAuthenticated() && session?.user?.name ? session.user.name : 'Login'}</div>
+          <img alt={'user'} src={isAuthenticated() ? imgUrl : defaultImg} className="mx-2 h-10 w-10 rounded-full object-cover" />
+          <div className="my- mx-4 text-xl">{isAuthenticated() && session?.user?.name ? session.user.name : 'Login'}</div>
         </div>
       </button>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,35 +2,36 @@ import Button from '@/components/common/Button';
 import Profile from '@/components/user/profile';
 import { NextPage } from 'next';
 import Link from 'next/link';
-import router from 'next/router';
 
 const CreateJury: NextPage = () => {
-  const imgUrl =
-    'https://scontent-muc2-1.xx.fbcdn.net/v/t31.18172-8/17492379_1395721013824677_2431623315541165382_o.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_ohc=HxVgLT_npx4AX-WRSrd&_nc_ht=scontent-muc2-1.xx&oh=00_AfDNqfcc7VuvR0-bjrGcEHQA4Om_dOKr7xiHiS2Hu6-7Fg&oe=645399B7';
-
   return (
     <>
       <div className="flex h-screen flex-col justify-center">
-        <div className="flex justify-center py-2">
-          <Link href={'/results/'}>
-            <Button text="Show Results" />
-          </Link>
+        {/* profile */}
+        <div className="hidden sm:block">
+          <div className="m-6 flex justify-end">
+            <Profile />
+          </div>
         </div>
-        <div className="flex justify-center py-2">
-          <Link href={'/jury/create'}>
-            <Button text="Create Jury" />
-          </Link>
-        </div>
-        <div className="flex justify-center py-2">
-          <Link href={'/'}>
-            <Button text="Join Jury" />
-          </Link>
-        </div>
-      </div>
 
-      {/* profile */}
-      <div className="absolute right-40 top-5">
-        <Profile imgUrl={imgUrl} />
+        {/* menu */}
+        <div className="flex flex-grow flex-col justify-center">
+          <div className="flex justify-center py-2">
+            <Link href={'/results/'}>
+              <Button text="Show Results" />
+            </Link>
+          </div>
+          <div className="flex justify-center py-2">
+            <Link href={'/jury/create'}>
+              <Button text="Create Jury" />
+            </Link>
+          </div>
+          <div className="flex justify-center py-2">
+            <Link href={'/'}>
+              <Button text="Join Jury" />
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   );
