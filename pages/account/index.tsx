6ccc0cb9f75352a7035174f3c4de854829f144e8
Nils Effinghausen <nils.effinghausen@telekom.de>
2023-06-20 18:16:42 +0200

    account: add update function for instagram handle

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -3,11 +3,35 @@ import router from 'next/router';
 import { useEffect, useState } from 'react';
 import { getSession, signOut } from 'next-auth/react';
 import Link from 'next/link';
+import TextInput from '@/components/common/TextInput';
+import ActionButton from '@/components/common/ActionButton';
+import { Action } from '@/types/enums/action';
 
 const defaultImg = '/profile/default-pfp.png';
 
 const Account = ({ session }: any) => {
   const [imageUrl, setImageUrl] = useState('');
+
+  const [instagramHandle, setInstagramHandle] = useState();
+
+  const handleSaveUserInfoClicked = async () => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/users`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        instagramHandle: instagramHandle,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${session.user.accessToken}`,
+      },
+    });
+
+    if (response.status == 200) {
+      console.log('updating user info successful');
+    } else {
+      console.log('updating user info failed');
+    }
+  };
 
   const handleBackToHomeClicked = () => {
     router.replace('/');
@@ -54,6 +78,28 @@ const Account = ({ session }: any) => {
           <Link href="/account/image">
             <img src={imageUrl ? imageUrl : defaultImg} className="mx-2 flex h-32 w-32 rounded-full object-cover" />
           </Link>
+        </div>
+
+        <div className="my-4" />
+
+        <div className={'flex columns-1 flex-col items-center'}>
+          <div className="m-2 flex flex-col rounded-lg bg-zinc-300 p-1">
+            <TextInput
+              id={'instagramHandle'}
+              label={'Instagram Handle'}
+              placeholder="@freestyler.kevin"
+              value={instagramHandle}
+              onChange={e => {
+                setInstagramHandle(e.currentTarget.value);
+              }}
+            />
+          </div>
+
+          <div className="my-2 flex">
+            <div className="px-1">
+              <ActionButton action={Action.SAVE} onClick={handleSaveUserInfoClicked} />
+            </div>
+          </div>
         </div>
 
         <div className="flex justify-center pt-10">
