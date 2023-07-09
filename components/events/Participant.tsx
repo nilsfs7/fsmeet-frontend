8e3003204f3038e138c34979d1afda3ee9a8afc9
Nils Effinghausen <nils.effinghausen@telekom.de>
2023-07-09 21:25:46 +0200

    participant list: fix field size

--- components/events/Participant.tsx
+++ components/events/Participant.tsx
@@ -9,9 +9,9 @@ const defaultImg = '/profile/user.png';
 const Participant = ({ participant }: IParticipantProps) => {
   return (
     <div className={'rounded-lg border-2 border-black bg-zinc-300 p-2 hover:bg-zinc-400'}>
-      <div className="grid grid-flow-col items-center justify-start">
-        <img src={participant.imageUrl ? participant.imageUrl : defaultImg} className="mx-2 h-6 w-6 rounded-full object-cover" />
-        <div>{participant.username}</div>
+      <div className="grid grid-flow-col items-center justify-between">
+        <img src={participant.imageUrl ? participant.imageUrl : defaultImg} className="mx-1 h-6 w-6 rounded-full object-cover" />
+        <div className="mx-1">{participant.username}</div>
       </div>
     </div>
   );
--- components/events/ParticipantList.tsx
+++ components/events/ParticipantList.tsx
@@ -12,12 +12,18 @@ const ParticipantList = ({ participants }: IParticipantListProps) => {
       <div className="text-base font-bold">Participants</div>
       <div className="flex flex-wrap">
         {participants.map((participant, i) => {
+          let margin = 'my-1 mx-1';
+          i === 0 ? (margin = 'm-1') : null;
+          i === participants.length - 1 ? (margin = 'ml-1') : null;
+
           return (
-            <div key={i} className="m-1 h-16 w-36">
-              <Link href={`/user/${participant.username}`}>
-                <Participant participant={participant} />
-              </Link>
-            </div>
+            <>
+              <div key={i} className={`my-1 ${margin}`}>
+                <Link href={`/user/${participant.username}`}>
+                  <Participant participant={participant} />
+                </Link>
+              </div>
+            </>
           );
         })}
       </div>
