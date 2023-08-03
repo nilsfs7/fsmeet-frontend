cee1a1ad7a6c5168de25ed283ed611a433ee7adf
Nils Effinghausen <nils.effinghausen@telekom.de>
2023-08-03 15:33:19 +0200

    feedback: improve page layout for selecting feedback type

--- pages/feedback/index.tsx
+++ pages/feedback/index.tsx
@@ -8,25 +8,23 @@ const feedbackImg = '/feedback/general-feedback.svg';
 
 const Feedback: NextPage = () => {
   return (
-    <>
-      <div className={'flex h-screen columns-1 flex-col items-center justify-center'}>
-        <div className="flex justify-center py-2">
-          <Link href="feedback/bugs">
-            <TextAndImageButton text="Report Bug" image={bugImg} />
-          </Link>
-        </div>
-        <div className="flex justify-center py-2">
-          <Link href="feedback/features">
-            <TextAndImageButton text="Request Feature" image={featureImg} />
-          </Link>
-        </div>
-        <div className="flex justify-center py-2">
-          <Link href="feedback/general">
-            <TextAndImageButton text="General Feedback" image={feedbackImg} />
-          </Link>
-        </div>
+    <div className={'absolute inset-0 flex flex-col items-center justify-center'}>
+      <div className="py-2">
+        <Link href="feedback/bugs">
+          <TextAndImageButton text="Report Bug" image={bugImg} />
+        </Link>
       </div>
-    </>
+      <div className="py-2">
+        <Link href="feedback/features">
+          <TextAndImageButton text="Request Feature" image={featureImg} />
+        </Link>
+      </div>
+      <div className="py-2">
+        <Link href="feedback/general">
+          <TextAndImageButton text="General Feedback" image={feedbackImg} />
+        </Link>
+      </div>
+    </div>
   );
 };
 
