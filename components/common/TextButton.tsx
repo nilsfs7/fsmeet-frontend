abc20c49316ff1f159e04c9fe86c0a2fdb729142
Nils Effinghausen <nils.effinghausen@telekom.de>
2023-08-13 11:20:34 +0200

    components: unify border style

--- components/common/TextButton.tsx
+++ components/common/TextButton.tsx
@@ -5,7 +5,7 @@ interface IButton {
 
 const TextButton = ({ text, onClick }: IButton) => {
   return (
-    <button className="w-36 rounded-lg border-[2px] border-black bg-zinc-300 p-2 font-bold text-black hover:bg-zinc-400" onClick={onClick}>
+    <button className="w-36 rounded-lg border-2 border-black bg-zinc-300 p-2 font-bold text-black hover:bg-zinc-400" onClick={onClick}>
       {text}
     </button>
   );
