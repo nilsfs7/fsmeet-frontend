9625f36c735d9ec2759e22f6af363ab94d6c9383
Nils Effinghausen <nils.effinghausen@telekom.de>
2023-09-01 08:09:34 +0200

    error handling: fix message styling

--- components/ErrorMessage.tsx
+++ components/ErrorMessage.tsx
@@ -4,7 +4,7 @@ interface IErrorMessageProps {
 
 const ErrorMessage = ({ message }: IErrorMessageProps) => {
   return message ? (
-    <div className="flex justify-center border border-black py-2">
+    <div className="flex justify-center py-2">
       <label className="text-dark-red">{`${message}`}</label>
     </div>
   ) : null;
