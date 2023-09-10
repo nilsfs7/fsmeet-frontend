f686c2518af1653dcd946a93109412e7097f9660
Nils Effinghausen <nils.effinghausen@telekom.de>
2023-09-10 21:21:35 +0200

    remove jury selection

--- components/common/OverlayButton.tsx
+++
@@ -1,9 +0,0 @@
-interface IButton {
-  text: string;
-}
-
-const OverlayButton = ({ text }: IButton) => {
-  return <button className="w-16 rounded-lg border border-black bg-primary-light text-sm font-bold text-black hover:bg-primary">{text}</button>;
-};
-
-export default OverlayButton;
