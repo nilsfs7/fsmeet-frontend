a52fcd556d824573f25706d88ddf159a52ae8fbb
Nils Effinghausen <nils.effinghausen@telekom.de>
2023-10-01 08:52:25 +0200

    registration: fix layout for pending and pw reset

--- pages/registration/pending.tsx
+++ pages/registration/pending.tsx
@@ -10,9 +10,9 @@ const RegistrationPending: NextPage = () => {
   return (
     <>
       <div className={'flex h-screen columns-1 flex-col items-center justify-center'}>
-        <div className="m-2">Check your mail</div>
-        <div>A confirmation link for {username} was sent to</div>
-        <div>{email}</div>
+        <div className="mx-2 text-center">Check your mail</div>
+        <div className="mx-2 text-center">A confirmation link for {username} was sent to</div>
+        <div className="mx-2 text-center">{email}</div>
 
         <div className="m-2 flex justify-center py-2">
           <Link href="/">
