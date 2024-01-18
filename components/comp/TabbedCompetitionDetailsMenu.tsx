cc32edb1640681cb11cbc7264a317fd393477058
Nils Effinghausen <nils.effinghausen@telekom.de>
2024-01-18 17:08:42 +0100

    comp details view: improve layout

--- components/comp/TabbedCompetitionDetailsMenu.tsx
+++ components/comp/TabbedCompetitionDetailsMenu.tsx
@@ -112,7 +112,9 @@ const TabbedCompetitionDetailsMenu = ({ competitionParticipants = [], descriptio
       {/* Battle Grid */}
       {rounds.length > 1 && (
         <TabsContent value="grid">
-          <BattleGrid rounds={rounds} usersMap={usersMap} />
+          <div className="overflow-x-auto">
+            <BattleGrid rounds={rounds} usersMap={usersMap} />
+          </div>
         </TabsContent>
       )}
 
