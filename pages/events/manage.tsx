6a7fffec459748100e27faa96bec3984986bcfdf
Nils Effinghausen <nils.effinghausen@telekom.de>
2024-02-19 22:13:03 +0100

    event management: remove confirm action from dialog (license warning)

--- pages/events/manage.tsx
+++ pages/events/manage.tsx
@@ -40,7 +40,7 @@ const MyEventsOverview = ({ data, session }: { data: any; session: any }) => {
 
   return (
     <>
-      <Dialog title="License Warning" queryParam="license" onCancel={handleCancelDialogClicked} onConfirm={handleConfirmDialogClicked}>
+      <Dialog title="License Warning" queryParam="license" onCancel={handleCancelDialogClicked}>
         <p>Out of licenses to create new events.</p>
         <p>
           Users can create a maximum of 2 events for now. By deleting any event that is not listed publicly, you can reclaim 1 license. Note that once an event is public it is not eligible for a
