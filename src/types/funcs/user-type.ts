05245494eb7b91db848004fe0cf649f85215d8a5
Nils Effinghausen <nils.effinghausen@telekom.de>
2024-04-17 17:44:00 +0200

    user profiles: add user type

---
+++ src/types/funcs/user-type.ts
@@ -0,0 +1,42 @@
+import { imgAssociation, imgBrand, imgDJ, imgFreestyler, imgMC, imgMedia, imgTechnical } from '../consts/images';
+import { UserType } from '../enums/user-type';
+
+export function getUserTypeLabels(userType: UserType): string {
+  switch (userType) {
+    case UserType.ASSOCIATION:
+      return 'Association';
+    case UserType.BRAND:
+      return 'Brand';
+    case UserType.DJ:
+      return 'DJ';
+    case UserType.FREESTYLER:
+      return 'Freestyler';
+    case UserType.MC:
+      return 'MC';
+    case UserType.MEDIA:
+      return 'Media';
+    case UserType.TECHNICAL:
+      return 'Technical';
+  }
+}
+
+export function getUserTypeImages(userType: UserType): { path: string; size: number } {
+  const defaultSize = 30;
+
+  switch (userType) {
+    case UserType.ASSOCIATION:
+      return { path: imgAssociation, size: 40 };
+    case UserType.BRAND:
+      return { path: imgBrand, size: defaultSize };
+    case UserType.DJ:
+      return { path: imgDJ, size: defaultSize };
+    case UserType.FREESTYLER:
+      return { path: imgFreestyler, size: 40 };
+    case UserType.MC:
+      return { path: imgMC, size: defaultSize };
+    case UserType.MEDIA:
+      return { path: imgMedia, size: defaultSize };
+    case UserType.TECHNICAL:
+      return { path: imgTechnical, size: defaultSize };
+  }
+}
