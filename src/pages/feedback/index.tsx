89e98c5df1aa0234a6447349a47b6ac3ca315ece
Nils Effinghausen <nils.effinghausen@telekom.de>
2024-04-23 19:07:05 +0200

    feedback: fix navigation layout

--- src/pages/feedback/index.tsx
+++ src/pages/feedback/index.tsx
@@ -12,18 +12,16 @@ import { GetServerSidePropsContext } from 'next';
 const Feedback = () => {
   return (
     <div className={'absolute inset-0 flex flex-col'}>
-      <div className="flex h-full flex-col items-center justify-center">
-        <div className="py-2">
+      <div className="p-2 h-full grid overflow-y-auto">
+        <div className="h-full flex flex-col items-center justify-center gap-2">
           <Link href="feedback/general">
             <TextAndImageButton text="General Feedback" image={imgFeedback} />
           </Link>
-        </div>
-        <div className="py-2">
+
           <Link href="feedback/features">
             <TextAndImageButton text="Request Feature" image={imgFeature} />
           </Link>
-        </div>
-        <div className="py-2">
+
           <Link href="feedback/bugs">
             <TextAndImageButton text="Report Bug" image={imgBug} />
           </Link>
