75d5860b434822cd72c05330da0edad7a5fe4f30
Nils Effinghausen <nils.effinghausen@telekom.de>
2024-06-23 15:29:19 +0200

    events: prioritize image loading

--- src/pages/events/notfound.tsx
+++ src/pages/events/notfound.tsx
@@ -1,14 +1,14 @@
 import { imgNotFound } from '@/types/consts/images';
 import Link from 'next/link';
 import Image from 'next/image';
-import { routeEvents, routeHome } from '@/types/consts/routes';
+import { routeEvents } from '@/types/consts/routes';
 import TextButton from '@/components/common/TextButton';
 
 const EventNotFound = () => {
   return (
     <div className={'absolute inset-0 flex flex-col items-center justify-center'}>
       <div className="py-2 ">
-        <Image src={imgNotFound} width={0} height={0} sizes="100vw" className={`h-12 w-full`} alt={'image'} />
+        <Image src={imgNotFound} width={0} height={0} sizes="100vw" className={`h-12 w-full`} alt={'image'} priority={true} />
         <div className="m-1 text-center text-lg font-bold">
           <div>{`Event not found.`}</div>
         </div>
