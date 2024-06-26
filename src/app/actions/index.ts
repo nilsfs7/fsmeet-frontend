661285e0e26c3b732133071c2f4903fc4fa52e1b
Nils Effinghausen <nils.effinghausen@telekom.de>
2024-06-26 19:51:31 +0200

    cleanup

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -3,11 +3,6 @@
 import { signIn, signOut } from '@/auth';
 import bcrypt from 'bcryptjs';
 
-// export async function doSocialLogin(formData: any) {
-//     const action = formData.get('action');
-//     await signIn(action, { redirectTo: "/" });
-// }
-
 export async function doLogout() {
   await signOut({ redirectTo: '/' });
 }
@@ -27,21 +22,3 @@ export async function doCredentialLogin(username: string, password: string) {
     throw err;
   }
 }
-
-// export async function doCredentialLogin(formData: any) {
-//   console.log("formData", formData);
-
-//   try {
-//     const hashedPassword = bcrypt.hashSync(formData.get("password"), '$2a$10$CwTycUXWue0Thq9StjUM0u');
-
-//     const response = await signIn("credentials", {
-//       usernameOrEmail: formData.get("username"),
-//       password: hashedPassword,
-//       redirect: false,
-//     });
-
-//     return response;
-//   } catch (err) {
-//     throw err;
-//   }
-// }
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -13,7 +13,6 @@ import TextButton from './common/TextButton';
 import TextInput from './common/TextInput';
 import { Toaster, toast } from 'sonner';
 import router from 'next/router';
-import { auth } from '@/auth';
 import { getSession } from 'next-auth/react';
 
 export const LoginForm = () => {
@@ -42,12 +41,6 @@ export const LoginForm = () => {
 
   const handleLoginClicked = async () => {
     const response = await doCredentialLogin(usernameOrEmail, password);
-
-    // const response = await signIn('credentials', {
-    //   usernameOrEmail: usernameOrEmail,
-    //   password: password,
-    //   redirect: false,
-    // });
 
     const session = await getSession();
 
@@ -85,51 +78,6 @@ export const LoginForm = () => {
     //     }
     //   };
   };
-
-  // const router = useRouter();
-  // const [error, setError] = useState("");
-
-  // async function onSubmit(event: any) {
-  //     event.preventDefault();
-  //     try {
-  //         const formData = new FormData(event.currentTarget);
-  //         const response = await doCredentialLogin(formData);
-
-  //         if (!!response.error) {
-  //             console.error(response.error);
-  //             setError(response.error.message);
-  //         } else {
-  //             router.push("/");
-  //         }
-  //     } catch (e) {
-  //         console.error(e);
-  //         setError("Check your Credentials");
-  //     }
-  // }
-
-  // return (
-  //     <>
-  //         <div className="text-xl text-red-500">{error}</div>
-  //         <form
-  //             className="my-5 flex flex-col items-center border p-3 border-gray-200 rounded-md"
-  //             onSubmit={onSubmit}>
-  //             <div className="my-2">
-  //                 <label htmlFor="username">Username</label>
-  //                 <input className="border mx-2 border-gray-500 rounded" type="text" name="username" id="username" />
-  //             </div>
-
-  //             <div className="my-2">
-  //                 <label htmlFor="password">Password</label>
-  //                 <input className="border mx-2 border-gray-500 rounded" type="password" name="password" id="password" />
-  //             </div>
-
-  //             <button type="submit" className="bg-orange-300 mt-4 rounded flex justify-center items-center w-36">
-  //                 Ceredential Login
-  //             </button>
-  //         </form>
-
-  //     </>
-  // );
 
   return (
     <>
