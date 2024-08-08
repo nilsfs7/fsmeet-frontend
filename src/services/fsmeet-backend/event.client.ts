0fa98f957e4ad4147ad53b95ad41890791f1d106
Nils Effinghausen <nils.effinghausen@telekom.de>
2024-08-08 13:27:33 +0200

    refactor: add function to event client

--- src/services/fsmeet-backend/event.client.ts
+++ src/services/fsmeet-backend/event.client.ts
@@ -237,6 +237,30 @@ export async function createSubComment(eventId: string, rootCommentId: string, m
   }
 }
 
+export async function createEventFeedback(eventId: string, message: string, session: Session | null): Promise<void> {
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/events/${eventId}/feedback`;
+
+  const body = JSON.stringify({
+    message: message,
+  });
+
+  const response = await fetch(url, {
+    method: 'POST',
+    body: body,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${session?.user?.accessToken}`,
+    },
+  });
+
+  if (response.ok) {
+    console.info('Creating event feedback successful');
+  } else {
+    const error = await response.json();
+    throw Error(error.message);
+  }
+}
+
 export async function updateEvent(event: Event, session: Session | null): Promise<void> {
   const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/events`;
 
