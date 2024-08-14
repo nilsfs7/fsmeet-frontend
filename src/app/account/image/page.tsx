190ca857f44796912ea0961c9b3ccac5e362e306
Nils Effinghausen <n.effinghausen@gmail.com>
2024-08-14 22:45:10 +0200

    migration: move /account/image to app dir

---
+++ src/app/account/image/page.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import Navigation from '@/components/Navigation';
+import ActionButton from '@/components/common/ActionButton';
+import { routeAccount, routeLogin } from '@/types/consts/routes';
+import { Action } from '@/types/enums/action';
+import { auth } from '@/auth';
+import PageTitle from '@/components/PageTitle';
+import { RedirectType, redirect } from 'next/navigation';
+import { ProfilePictureUpload } from './components/profile-picture-upload';
+
+export default async function AccountImage() {
+  const session = await auth();
+
+  // TODO: remove because redirect is done by middleware anyway
+  if (!session?.user?.username) {
+    redirect(routeLogin, RedirectType.replace);
+  }
+
+  return (
+    <div className="h-[calc(100dvh)] flex flex-col">
+      <PageTitle title="Account Settings" />
+
+      <ProfilePictureUpload />
+
+      <Navigation>
+        <Link href={routeAccount}>
+          <ActionButton action={Action.BACK} />
+        </Link>
+      </Navigation>
+    </div>
+  );
+}
