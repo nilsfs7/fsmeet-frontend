91f7eaf9ea6f3345f699696553294a5c96c5b8dd
Nils Effinghausen <n.effinghausen@gmail.com>
2024-10-23 15:35:41 +0200

    migrate: events/eventid/not-found

--- src/app/events/[eventId]/not-found.tsx
+++ src/app/events/[eventId]/not-found.tsx
@@ -3,24 +3,24 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { routeEvents } from '@/domain/constants/routes';
 import TextButton from '@/components/common/TextButton';
+import { getTranslations } from 'next-intl/server';
 
-const EventNotFound = () => {
+export default async function NotFound() {
+  const t = await getTranslations('/events/eventid/not-found');
   return (
     <div className={'absolute inset-0 flex flex-col items-center justify-center'}>
       <div className="py-2 ">
         <Image src={imgNotFound} width={0} height={0} sizes="100vw" className={`h-12 w-full`} alt={'image'} priority={true} />
         <div className="m-1 text-center text-lg font-bold">
-          <div>{`Event not found.`}</div>
+          <div>{t('textNotFound')}</div>
         </div>
       </div>
 
       <div className="py-2">
         <Link href={routeEvents}>
-          <TextButton text="Back To Overview" />
+          <TextButton text={t('btnBackToEvents')} />
         </Link>
       </div>
     </div>
   );
-};
-
-export default EventNotFound;
+}
