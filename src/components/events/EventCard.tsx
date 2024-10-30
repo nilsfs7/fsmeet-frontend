ca5dbd015fd145d7f2269241af2781954ba801db
Nils Effinghausen <n.effinghausen@gmail.com>
2024-10-30 15:49:12 +0100

    translation: event card

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { imgCompetition, imgMeeting } from '@/domain/constants/images';
 import { getShortDateString } from '@/functions/time';
 import Separator from '../Seperator';
@@ -5,15 +7,17 @@ import { Event } from '@/types/event';
 import moment from 'moment';
 import { EventType } from '@/domain/enums/event-type';
 import { isPublicEventState } from '@/functions/is-public-event-state';
+import { useTranslations } from 'next-intl';
 
 interface IEventProps {
   event: Event;
 }
 
 const EventCard = ({ event }: IEventProps) => {
+  const t = useTranslations('global/components/event-card');
+
   return (
     <div className={'rounded-lg border border-secondary-dark bg-secondary-light p-2 text-sm hover:border-primary'}>
-      {/* top */}
       <div className={'max-h-24 p-2'}>
         <div className="28 flex ">
           <div className="w-full text-base font-bold">{`${!isPublicEventState(event.state) ? '[NOT LISTED] ' : ''}${event.name}`}</div>
@@ -29,18 +33,17 @@ const EventCard = ({ event }: IEventProps) => {
           )}
         </div>
         <div className="flex justify-between">
-          <div className="text-xs">by {event.admin}</div>
-          <div>{event.registrationDeadline && <div className="text-right">Deadline: {getShortDateString(moment(event.registrationDeadline))}</div>}</div>
+          <div className="text-xs">{`${t('lblHost')}: ${event.admin}`}</div>
+          <div>{event.registrationDeadline && <div className="text-right">{`${t('lblDeadline')}: ${getShortDateString(moment(event.registrationDeadline))}`}</div>}</div>
         </div>
       </div>
 
       <Separator />
 
-      {/* botton */}
       <div className="flex h-20 p-2">
         <p className="h-full w-2/3 overflow-hidden text-ellipsis">{event.description}</p>
         <div className="flex h-full w-1/3 items-center justify-end">
-          <img className="h-full" src={event.type === 'meet' ? imgMeeting : imgCompetition} alt={'event image'}></img>
+          <img className="h-full" src={event.type === EventType.MEETING ? imgMeeting : imgCompetition} alt={'event image'}></img>
         </div>
       </div>
     </div>
