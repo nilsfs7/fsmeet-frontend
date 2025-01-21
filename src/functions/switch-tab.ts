5f15377dd2061ef53f34d0d97d09d20430a3e5b9
Nils Effinghausen <nils.effinghausen@stakingfacilities.com>
2025-01-21 07:20:13 +0100

    cleanup

--- src/functions/switch-tab.ts
+++ src/functions/switch-tab.ts
@@ -1,25 +1,9 @@
 'use client';
 
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
-import { NextRouter } from 'next/router';
 
 export function switchTab(router: AppRouterInstance, newTab: string): void {
   const params = new URLSearchParams(window.location.search);
   params.set('tab', newTab);
   router.replace(`${window.location.pathname}?${params.toString()}`, { scroll: false });
-}
-
-// TODO: remove after migration to app router
-export function switchTab_pages(router: NextRouter, newTab: string): void {
-  if (router.asPath.includes('tab=')) {
-    const { tab } = router.query;
-
-    router.replace(`${router.asPath}`.replace(`tab=${tab}`, `tab=${newTab}`), undefined, { shallow: true });
-  } else {
-    if (router.asPath.includes('?')) {
-      router.replace(`${router.asPath}&tab=${newTab}`, undefined, { shallow: true });
-    } else {
-      router.replace(`${router.asPath}?tab=${newTab}`, undefined, { shallow: true });
-    }
-  }
 }
