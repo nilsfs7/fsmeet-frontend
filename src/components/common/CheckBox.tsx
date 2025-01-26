41be6142dd505399b28344750eb50cba21886553
Nils Effinghausen <nils.effinghausen@stakingfacilities.com>
2025-01-26 03:52:18 +0100

    checkbox: add attribute "disabled"

--- src/components/common/CheckBox.tsx
+++ src/components/common/CheckBox.tsx
@@ -2,14 +2,15 @@ interface ICheckBoxInput {
   id: string;
   label: string;
   value?: boolean;
+  disabled?: boolean;
   onChange?: (event: any) => void;
 }
 
-const CheckBox = ({ id, label, value, onChange }: ICheckBoxInput) => {
+const CheckBox = ({ id, label, value, disabled = false, onChange }: ICheckBoxInput) => {
   return (
     <div className="m-2 grid grid-cols-2 place-items-start items-center">
       <div className="py-1">{label}</div>
-      <input id={id} className="h-4 w-4" type="checkbox" checked={value} onChange={onChange} />
+      <input id={id} className="h-4 w-4" type="checkbox" checked={value} disabled={disabled} onChange={onChange} />
     </div>
   );
 };
