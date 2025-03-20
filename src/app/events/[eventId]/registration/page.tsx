8980b22da0b9c50b82be2011b61fd8d43fcedd7e
Nils Effinghausen <nils.effinghausen@stakingfacilities.com>
2025-03-20 08:55:04 +0100

    registration v2: redirect if not logged in

--- src/app/events/[eventId]/registration/page.tsx
+++ src/app/events/[eventId]/registration/page.tsx
@@ -2,9 +2,18 @@ import { auth } from '@/auth';
 import { getEvent } from '@/infrastructure/clients/event.client';
 import { EventRegistrationProcess } from './components/event-registration-process';
 import { getUser } from '@/infrastructure/clients/user.client';
+import { redirect } from 'next/navigation';
+import { routeEvents, routeLogin } from '@/domain/constants/routes';
+import { validateSession } from '@/functions/validate-session';
 
 export default async function EventRegistration({ params }: { params: { eventId: string } }) {
   const session = await auth();
+
+  const loginRouteWithCallbackUrl = `${routeLogin}?callbackUrl=${routeEvents}%2F${params.eventId}%2Fregistration`;
+
+  if (!validateSession(session)) {
+    redirect(loginRouteWithCallbackUrl);
+  }
 
   const event = await getEvent(params.eventId, session);
   const user = await getUser(session?.user.username ? session.user.username : '', session);
