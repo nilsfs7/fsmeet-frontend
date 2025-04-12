b647d8f677b7055bd54c4a9f2de0b25bc71273f7
Nils Effinghausen <nils.effinghausen@stakingfacilities.com>
2025-04-12 22:25:03 +0200

    dpendencies: update minor

--- src/functions/get-country-name-by-code.ts
+++ src/functions/get-country-name-by-code.ts
@@ -3,5 +3,5 @@ import { countries } from 'countries-list';
 export function getCountryNameByCode(code: string): string {
   // @ts-ignore: next-line
   const country = countries[code.toUpperCase()];
-  return country ? country.name : null;
+  return country ? country.name : '';
 }
