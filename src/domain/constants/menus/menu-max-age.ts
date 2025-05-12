c56ce0998eae362598f88570a654f0e72c7a69ec
Nils Effinghausen <nils.effinghausen@stakingfacilities.com>
2025-05-12 14:31:39 +0200

    more granular max age

--- src/domain/constants/menus/menu-max-age.ts
+++ src/domain/constants/menus/menu-max-age.ts
@@ -4,8 +4,12 @@ import { MaxAge } from '@/domain/enums/max-age';
 export const menuMaxAge: MenuItem[] = [
   { text: 'None', value: MaxAge.NONE.toString() },
   { text: '12', value: MaxAge.TWELVE.toString() },
+  { text: '13', value: MaxAge.THIRTEEN.toString() },
   { text: '14', value: MaxAge.FOURTEEN.toString() },
+  { text: '15', value: MaxAge.FIFTEEN.toString() },
   { text: '16', value: MaxAge.SIXTEEN.toString() },
+  { text: '17', value: MaxAge.SEVENTEEN.toString() },
   { text: '18', value: MaxAge.EIGHTEEN.toString() },
+  { text: '19', value: MaxAge.NINETEEN.toString() },
   { text: '20', value: MaxAge.TWENTY.toString() },
 ];
