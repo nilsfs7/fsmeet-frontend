82506635b4fb10c375aba0cb94774e8f8c73125b
Nils Effinghausen <nils.effinghausen@stakingfacilities.com>
2025-06-28 11:03:23 +0200

    checkbox: improve layout

--- src/components/common/CheckBox.tsx
+++ src/components/common/CheckBox.tsx
@@ -8,7 +8,7 @@ interface ICheckBoxInput {
 
 const CheckBox = ({ id, label, value, disabled = false, onChange }: ICheckBoxInput) => {
   return (
-    <div className="m-2 grid grid-cols-2 place-items-start items-center">
+    <div className="m-2 grid grid-cols-2 place-items-start items-center gap-2">
       <div className="py-1">{label}</div>
       <input id={id} className="h-4 w-4" type="checkbox" checked={value} disabled={disabled} onChange={onChange} />
     </div>
