48b25238c2dc6cbb8268d46403bbd306df470610
Nils Effinghausen <nils.effinghausen@stakingfacilities.com>
2025-07-07 18:08:21 +0200

    voice: add tool tip to "copy url btn"

--- src/app/voice/components/action-button-copy-poll-url.tsx
+++ src/app/voice/components/action-button-copy-poll-url.tsx
@@ -4,17 +4,20 @@ import { Toaster, toast } from 'sonner';
 import ActionButton from '@/components/common/ActionButton';
 import { Action } from '@/domain/enums/action';
 import { copyToClipboard } from '@/functions/copy-to-clipboard';
+import { useTranslations } from 'next-intl';
 
 export const ActionButtonCopyPollUrl = () => {
+  const t = useTranslations('/voice');
+
   const handleCopyClicked = async () => {
     copyToClipboard(window.location.toString());
-    toast.info('Poll URL copied to clipboard.');
+    toast.info(t('btnCopyUrlToast'));
   };
 
   return (
     <>
       <Toaster richColors />
-      <ActionButton action={Action.SHARE} onClick={handleCopyClicked} />
+      <ActionButton action={Action.SHARE} onClick={handleCopyClicked} tooltip={t('btnCopyUrlToolTip')} />
     </>
   );
 };
