bba1ec336f0a88b3f7a7db84facd5b62f5df1576
Nils Effinghausen <nils.effinghausen@stakingfacilities.com>
2025-07-07 18:02:56 +0200

    map: add tool tip to "copy url btn"

--- src/app/map/components/action-button-copy-url.tsx
+++ src/app/map/components/action-button-copy-url.tsx
@@ -3,18 +3,21 @@
 import ActionButton from '@/components/common/ActionButton';
 import { Action } from '@/domain/enums/action';
 import { copyToClipboard } from '@/functions/copy-to-clipboard';
+import { useTranslations } from 'next-intl';
 import { Toaster, toast } from 'sonner';
 
 export const ActionButtonCopyUrl = () => {
+  const t = useTranslations('/map');
+
   const handleCopyClicked = async () => {
     copyToClipboard(window.location.toString());
-    toast.info('Map URL copied to clipboard.');
+    toast.info(t('btnCopyUrlToast'));
   };
 
   return (
     <>
       <Toaster richColors />
-      <ActionButton action={Action.SHARE} onClick={handleCopyClicked} />
+      <ActionButton action={Action.SHARE} onClick={handleCopyClicked} tooltip={t('btnCopyUrlToolTip')} />
     </>
   );
 };
