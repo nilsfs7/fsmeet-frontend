1e382ca4dffb7bc8070bbfaafc20cc92b0288641
Nils Effinghausen <nils.effinghausen@stakingfacilities.com>
2025-07-24 14:11:34 +0200

    event category: remove rbss

--- src/domain/constants/menus/menu-event-categories.ts
+++ src/domain/constants/menus/menu-event-categories.ts
@@ -3,10 +3,9 @@ import { EventCategory } from '@/domain/enums/event-category';
 
 export const menuEventCategories: MenuItem[] = [
   { text: 'Continental', value: EventCategory.CONTINENTAL },
-  { text: 'International', value: EventCategory.INTERNATIONAL },
+  { text: 'International Open', value: EventCategory.INTERNATIONAL },
   { text: 'National', value: EventCategory.NATIONAL },
   { text: 'Pulse', value: EventCategory.PULSE },
-  { text: 'RBSS', value: EventCategory.RBSS },
   { text: 'Super Ball', value: EventCategory.SUPERBALL },
   { text: 'WFFC', value: EventCategory.WFFC },
 ];
