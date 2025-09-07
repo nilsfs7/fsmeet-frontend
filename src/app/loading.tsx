86e56609423cea7d23057a7752345c97b5f29dde
Nils Effinghausen <nils.effinghausen@stakingfacilities.com>
2025-09-07 20:14:27 +0200

    drop unused loading spinner page

--- src/app/loading.tsx
+++
@@ -1,8 +0,0 @@
-import LoadingSpinner from '@/components/animation/loading-spinner';
-import { getTranslations } from 'next-intl/server';
-
-export default async function LoadingPage() {
-  const t = await getTranslations('/loading');
-
-  return <LoadingSpinner text={t('textLoading')} />;
-}
