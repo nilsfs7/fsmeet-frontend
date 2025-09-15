8ee43bd3ac84ff6389ca1d9594b477161e773617
Nils Effinghausen <nils.effinghausen@stakingfacilities.com>
2025-09-15 18:18:25 +0200

    statistics: add growth of freestylers

--- src/infrastructure/clients/dtos/event/read-user-growth.response.dto.ts
+++ src/infrastructure/clients/dtos/event/read-user-growth.response.dto.ts
@@ -5,11 +5,15 @@ export class ReadUserGrowthResponseDto {
   total: number;
   malesCount: number;
   femalesCount: number;
+  maleFreestylersCount: number;
+  femaleFreestylersCount: number;
 
-  constructor(date: Moment, total: number, malesCount: number, femalesCount: number) {
+  constructor(date: Moment, total: number, malesCount: number, femalesCount: number, maleFreestylersCount: number, femaleFreestylersCount: number) {
     this.date = date;
     this.total = total;
     this.malesCount = malesCount;
     this.femalesCount = femalesCount;
+    this.maleFreestylersCount = maleFreestylersCount;
+    this.femaleFreestylersCount = femaleFreestylersCount;
   }
 }
