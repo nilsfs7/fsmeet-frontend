e291f9d72a13a7db9241b5c14aa0943b31d57f89
Nils Effinghausen <n.effinghausen@gmail.com>
2026-01-12 14:19:39 +0100

    cleanup: remove unused dtos

--- src/infrastructure/clients/dtos/poll/read-poll-option.response.dto.ts
+++
@@ -1,9 +0,0 @@
-export class ReadPollOptionResponseDto {
-  option: string;
-  numVotes: number;
-
-  constructor(option: string, numVotes: number) {
-    this.option = option;
-    this.numVotes = numVotes;
-  }
-}
