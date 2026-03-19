7e6dac69427a8a41f7f0c8a90fcecb681c87a17c
Nils Effinghausen <n.effinghausen@gmail.com>
2026-03-19 20:48:07 +0100

    /account: add spanish and french to preferred language selection

--- src/domain/constants/menus/menu-supported-languages.ts
+++ src/domain/constants/menus/menu-supported-languages.ts
@@ -2,5 +2,7 @@ import { MenuItem } from '@/domain/types/menu-item';
 
 export const menuSupportedLanguages: MenuItem[] = [
   { text: 'English', value: 'en' },
+  { text: 'French', value: 'fr' },
+  { text: 'Spanish', value: 'es' },
   { text: 'Japanese', value: 'ja' },
 ];
