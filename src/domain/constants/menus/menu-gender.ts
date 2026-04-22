f9fdbb24f0b58bd3c767fb1693bd9ef9697c7a4f
Nils Effinghausen <n.effinghausen@gmail.com>
2026-04-22 22:44:48 +0200

    cleanup: drop menuGenderWithUnspecified

--- src/domain/constants/menus/menu-gender.ts
+++ src/domain/constants/menus/menu-gender.ts
@@ -5,10 +5,3 @@ export const menuGender: MenuItem[] = [
   { text: 'Female', value: Gender.FEMALE },
   { text: 'Male', value: Gender.MALE },
 ];
-
-// TODO: remove menuGenderWithUnspecified. Only here until every user has a gender set
-export const menuGenderWithUnspecified: MenuItem[] = [
-  { text: 'not specified', value: '--' },
-  { text: 'Female', value: Gender.FEMALE },
-  { text: 'Male', value: Gender.MALE },
-];
