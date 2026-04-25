69158849da3def9a89db6466d1fd054f63a9389f
Nils Effinghausen <n.effinghausen@gmail.com>
2026-04-25 13:52:42 +0200

    comp pool: refactor design

--- src/app/events/[eventId]/comps/[compId]/edit/pool/page.tsx
+++ src/app/events/[eventId]/comps/[compId]/edit/pool/page.tsx
@@ -9,6 +9,9 @@ import { getCompetition } from '@/infrastructure/clients/competition.client';
 import { getTranslations } from 'next-intl/server';
 import { getEvent } from '@/infrastructure/clients/event.client';
 import { auth } from '@/auth';
+import { cn } from '@/lib/utils';
+
+const eventDetailsContentClass = 'mx-auto w-full max-w-3xl min-w-0 px-3 sm:px-4';
 
 export default async function CompetitionPool(props: { params: Promise<{ eventId: string; compId: string }> }) {
   const params = await props.params;
@@ -19,10 +22,14 @@ export default async function CompetitionPool(props: { params: Promise<{ eventId
   const competition: Competition = await getCompetition(params.compId);
 
   return (
-    <div className="min-h-0 flex-1 flex flex-col">
-      <PageTitle title={t('pageTitle')} />
+    <div className="flex min-h-0 flex-1 flex-col">
+      <div className={cn('mt-2', eventDetailsContentClass)}>
+        <PageTitle title={t('pageTitle')} />
+      </div>
 
-      <Participants event={event} competition={competition} />
+      <div className={cn('mt-2 min-h-0 flex-1 overflow-y-auto', eventDetailsContentClass)}>
+        <Participants event={event} competition={competition} />
+      </div>
 
       <Navigation>
         <ActionButton href={`${routeEvents}/${params.eventId}/comps`} action={Action.BACK} />
