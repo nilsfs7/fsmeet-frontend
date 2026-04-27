b47cc6ccf655e73eaac2da0b94367e30acc345c6
Nils Effinghausen <n.effinghausen@gmail.com>
2026-04-27 08:30:55 +0200

    cleanup: drop unused files

--- src/components/common/section-header.tsx
+++
@@ -1,9 +0,0 @@
-interface ISectionHeader {
-  label: string;
-}
-
-const SectionHeader = ({ label }: ISectionHeader) => {
-  return <div className="mx-2 text-lg underline">{label}</div>;
-};
-
-export default SectionHeader;
