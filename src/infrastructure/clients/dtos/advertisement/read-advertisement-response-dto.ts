6d81ea86db4d9a7f7becca7bd0aff669731fd025
Nils Effinghausen <n.effinghausen@gmail.com>
2026-05-09 11:49:32 +0200

    advertisement client: filter by username

--- src/infrastructure/clients/dtos/advertisement/read-advertisement-response-dto.ts
+++ src/infrastructure/clients/dtos/advertisement/read-advertisement-response-dto.ts
@@ -6,8 +6,9 @@ export class ReadAdvertisementResponseDto {
   imageUrl: string;
   displayOrder: number;
   enabled: boolean;
+  username: string;
 
-  constructor(id: string, title: string, description: string, targetUrl: string, imageUrl: string, displayOrder: number, enabled: boolean) {
+  constructor(id: string, title: string, description: string, targetUrl: string, imageUrl: string, displayOrder: number, enabled: boolean, username: string) {
     this.id = id;
     this.title = title;
     this.description = description;
@@ -15,5 +16,6 @@ export class ReadAdvertisementResponseDto {
     this.imageUrl = imageUrl;
     this.displayOrder = displayOrder;
     this.enabled = enabled;
+    this.username = username;
   }
 }
